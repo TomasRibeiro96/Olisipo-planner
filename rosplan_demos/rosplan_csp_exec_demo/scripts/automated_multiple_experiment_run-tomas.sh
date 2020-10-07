89f9f10110424517b5d1446001aeacfffed7bbf5
tribeiro <tomasribeiro.pt@gmail.com>
2020-10-07 19:32:02 +0100

    Improved bash file

--- rosplan_demos/rosplan_csp_exec_demo/scripts/automated_multiple_experiment_run-tomas.sh
+++ rosplan_demos/rosplan_csp_exec_demo/scripts/automated_multiple_experiment_run-tomas.sh
@@ -5,17 +5,6 @@
 
 # NOTE: domain is same for all experiments: domain_robot_delivery.pddl
 
-# function call_exec_experiment(){
-#     # make experiments in parallel
-#     for i in `seq 0 9`; # do not set more than 9!
-#         do
-#             PORT=11${i}11
-#             export ROS_MASTER_URI=http://localhost:$PORT # roscore -p $PORT not needed!
-#             echo "roslaunch rosplan_csp_exec_demo rosplan_csp_exec_demo.launch adaptable_plan_dispatcher_required:=$1 category:=$2 problem:=$3 domain:=$4"
-#             timeout 600 roslaunch rosplan_csp_exec_demo rosplan_csp_exec_demo.launch adaptable_plan_dispatcher_required:=$1 category:=$2 problem:=$3 domain:=$4&
-#         done
-# }
-
 
 # arg 1: adaptable_plan_dispatcher_required options: (true, false)
 # arg 2: category, temporal or non temporal? options: (iros_problems_free, iros_problems_deadlines)
@@ -25,7 +14,6 @@ function call_exec_experiment_with_wait(){
     export ROS_MASTER_URI=http://localhost:$PORT # roscore -p $PORT not needed!
     # ./procs[${i}] &
     echo "roslaunch rosplan_csp_exec_demo rosplan_csp_exec_demo.launch adaptable_plan_dispatcher_required:=$1 category:=$2 domain:=$3 number_machines:=$4 probabilities_problem:=$5"
-    # timeout 600 roslaunch rosplan_csp_exec_demo rosplan_csp_exec_demo.launch adaptable_plan_dispatcher_required:=$1 category:=$2 problem:=$3 domain:=$4 probabilities:=$5&
     timeout 600 roslaunch rosplan_csp_exec_demo rosplan_csp_exec_demo.launch adaptable_plan_dispatcher_required:=$1 category:=$2 domain:=$3 number_machines:=$4 probabilities_problem:=$5
     # run processes and store pids in array
     pids[${i}]=$!
@@ -40,34 +28,40 @@ function call_exec_experiment_with_wait(){
 }
 
 
-# echo "${nt_prob[1]}" # test
+declare -a number_machines_vec=("3" "4")
+declare -a number_machines=${number_machines_vec[0]}
+
+declare -a number_runs=$((1))
+declare -a exp_count=$((0))
+declare -a total_count=$((2*7*1))
+
 
 #### With esterel dispatcher ####
-# Iterate over machines problems
-for number_machines in {3..5}
-do  
-    # Iterate over probabilities problem
-    for problem in {1..7}
+# Iterate over probabilities problem
+for problem in {1..7}
+do
+    # Number of runs for each pair of problems and probabilities
+    for k in {1..$number_runs}
     do
-        # Number of runs for each pair of problems and probabilities
-        for k in {0..1}
-        do
-            call_exec_experiment_with_wait false factory_robot_problems domain_simple_factory_robot $number_machines $problem
-        done
+        call_exec_experiment_with_wait false factory_robot_problems domain_simple_factory_robot $number_machines $problem
+        exp_count=$(($exp_count + 1))
+        echo ""
+        echo "|||||||||||||||||||||||||||||||||| EXECUTED $exp_count OF $total_count ||||||||||||||||||||||||||||||||||"
+        echo ""
     done
 done
 
 #### With adaptable dispatcher ####
-# Iterate over machines problems
-for number_machines in {3..5}
-do  
-    # Iterate over probabilities problem
-    for problem in {1..7}
+# Iterate over probabilities problem
+for problem in {1..7}
+do
+    # Number of runs for each pair of problems and probabilities
+    for k in {1..$number_runs}
     do
-        # Number of runs for each pair of problems and probabilities
-        for k in {0..1}
-        do
-            call_exec_experiment_with_wait true factory_robot_problems domain_simple_factory_robot $number_machines $problem
-        done
+        call_exec_experiment_with_wait true factory_robot_problems domain_simple_factory_robot $number_machines $problem
+        exp_count=$(($exp_count + 1))
+        echo ""
+        echo "|||||||||||||||||||||||||||||||||| EXECUTED $exp_count OF $total_count ||||||||||||||||||||||||||||||||||"
+        echo ""
     done
 done
